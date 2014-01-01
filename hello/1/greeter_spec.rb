8ac6a60099b5920c3fcce9dea2a3dd66cb44e1ab
thinkAmi <dev.thinkami@gmail.com>
2014-01-01 21:59:26 +0000

    p13

--- hello/1/greeter_spec.rb
+++ hello/1/greeter_spec.rb
@@ -1,7 +1,13 @@
+class RSpecGreeter
+  def greet
+    "Hello RSpec!"
+  end
+end
+
 describe "RSpec Greeter" do
 	it "should say 'Hello RSpec!' when it receives the greet() message" do
     greeter = RSpecGreeter.new          # Given
-    greeting = greeter.greeter          # When
-    greeting.should == "Hello RSpec"    # Then
+    greeting = greeter.greet            # When
+    greeting.should == "Hello RSpec!"   # Then
   end
 end
