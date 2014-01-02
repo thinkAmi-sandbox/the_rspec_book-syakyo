08cfda599528c1937817061818ecabe365bd6737
thinkAmi <dev.thinkami@gmail.com>
2014-01-02 23:46:22 +0000

    p83

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -11,20 +11,23 @@ module Codebreaker
     end
 
     def guess(guess)
-      exact_match_count = 0
       number_match_count = 0
-      mark = ''
-      (0..3).each do |index|
-        if exact_match?(guess, index)
-          exact_match_count += 1
-        end
-      end
       (0..3).each do |index|
         if number_match?(guess, index)
           number_match_count += 1
         end
       end
       @output.puts '+'*exact_match_count + '-'*number_match_count
+    end
+
+    def exact_match_count
+      exact_match_count = 0
+      (0..3).each do |index|
+        if exact_match?(guess, index)
+          exact_match_count += 1
+        end
+      end
+      exact_match_count
     end
 
     def exact_match?(guess ,index)
