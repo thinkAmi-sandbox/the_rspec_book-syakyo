8ef3e207a428cd11e68b9333882563c2ce8e7ff8
thinkAmi <dev.thinkami@gmail.com>
2014-01-03 00:03:51 +0000

    p88-413

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -8,23 +8,23 @@ module Codebreaker
 
       def exact_match_count(guess)
         (0..3).inject(0) do |count, index|
-          count + (exact_match?(guess, index) ? 1 : 0)
+          count + (exact_match?(@guess, index) ? 1 : 0)
         end
       end
 
       def number_match_count(guess)
         (0..3).inject(0) do |count, index|
-          count + (number_match?(guess, index) ? 1 : 0)
+          count + (number_match?(@guess, index) ? 1 : 0)
         end
       end
 
 
       def exact_match?(guess ,index)
-        guess[index] == @secret[index]
+        @guess[index] == @secret[index]
       end
 
       def number_match?(guess, index)
-        @secret.include?(guess[index]) && !exact_match?(guess, index)
+        @secret.include?(@guess[index]) && !exact_match?(@guess, index)
       end
 
     end
