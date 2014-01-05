f1372b2c187fcfd33fd00484c7ef5725f1072d57
thinkAmi <dev.thinkami@gmail.com>
2014-01-05 21:00:14 +0000

    p331-12

--- messages/spec/views/messages/new.html.erb_spec.rb
+++ messages/spec/views/messages/new.html.erb_spec.rb
@@ -1,8 +1,15 @@
 require 'spec_helper'
 
 describe "messages/new.html.erb" do
+  let(:message) do
+    mock_model("Message").as_new_record.as_null_object
+  end
+
+  before do
+    assign(:message, message)
+  end
+
   it "renders a form to create a message" do
-    assign(:message, mock_model("Message").as_new_record.as_null_object)
     render
     rendered.should have_selector("form",
       :method => "post",
@@ -13,10 +20,7 @@ describe "messages/new.html.erb" do
   end
 
   it "renders a text field for the message title" do
-    assign(
-      :message,
-      mock_model("Message", :title => "the title").as_new_record
-    )
+    message.stub(:title => "the title")
     render
     rendered.should have_selector("form") do |form|
       form.should have_selector("input",
