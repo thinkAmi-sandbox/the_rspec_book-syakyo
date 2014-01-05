27e87d31bb624937411166136e2a4217d8f87ade
thinkAmi <dev.thinkami@gmail.com>
2014-01-05 21:17:36 +0000

    p335-3

--- messages/spec/helpers/application_helper_spec.rb
+++ messages/spec/helpers/application_helper_spec.rb
@@ -10,5 +10,14 @@ describe ApplicationHelper do
         content.should == "content"
       end
     end
+
+    context "when the current user does not have the role" do
+      it "does not display the content" do
+        user = stub('User', :in_role? => false)
+        helper.stub(:current_user).and_return(user)
+        content = helper.display_for(:existing_role) {"content"}
+        content.should == nil
+      end
+    end
   end
 end
