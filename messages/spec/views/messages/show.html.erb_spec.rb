6c42cab44c486cae83294de6da50b0668a1d2ae8
thinkAmi <dev.thinkami@gmail.com>
2014-01-05 20:37:04 +0000

    p326

--- messages/spec/views/messages/show.html.erb_spec.rb
+++ messages/spec/views/messages/show.html.erb_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe "messages/show.html.erb" do
   it "displays the text attribute of the message" do
+    assign(:message, double("Message", :text => "Hello world!"))
     render
-    rendere.should contain("Hello world!")
+    render.should contain("Hello world!")
   end
 end
