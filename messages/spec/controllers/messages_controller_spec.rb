0b801f93065408ccaf01eab6fe25c799766f8336
thinkAmi <dev.thinkami@gmail.com>
2014-01-06 20:29:22 +0000

    p341-2

--- messages/spec/controllers/messages_controller_spec.rb
+++ messages/spec/controllers/messages_controller_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 
 describe MessagesController do
   describe "POST create" do
-    it "creates a new message"
+    it "creates a new message" do
+      Message.should_receive(:new).with("text" => "a quick brown fox")
+      post :create, :message => { "text" => "a quick brown fox" }
+    end
+    
     it "save the message"
   end
 end
