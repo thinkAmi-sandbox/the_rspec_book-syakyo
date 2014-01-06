0377487646eacb3ecf0a08f3bab48e635a21de0f
thinkAmi <dev.thinkami@gmail.com>
2014-01-06 20:35:25 +0000

    p343-6

--- messages/spec/controllers/messages_controller_spec.rb
+++ messages/spec/controllers/messages_controller_spec.rb
@@ -3,10 +3,16 @@ require 'spec_helper'
 describe MessagesController do
   describe "POST create" do
     it "creates a new message" do
+      pending("drive out redirect")
       Message.should_receive(:new).with("text" => "a quick brown fox")
       post :create, :message => { "text" => "a quick brown fox" }
     end
-    
+
     it "save the message"
+
+    it "redirects to the Messages index" do
+      post :create
+      response.should redirect_to(:action => "index")
+    end
   end
 end
