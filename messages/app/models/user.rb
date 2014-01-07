1da5d786bbf51dedb8b6cbec184b66c2b8ed33c3
thinkAmi <dev.thinkami@gmail.com>
2014-01-07 21:27:41 +0000

    p370

---
+++ messages/app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  has_many :received_message, :class_name => Message.name,
+    :foreign_key => "recipient_id"
+
+  def send_message(message_attrs)
+    Message.create! message_attrs
+  end
+end
--- messages/db/schema.rb
+++ messages/db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140107205322) do
+ActiveRecord::Schema.define(:version => 20140107212707) do
 
   create_table "messages", :force => true do |t|
     t.string   "title"
     t.text     "text"
     t.integer  "recipient_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "login"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
