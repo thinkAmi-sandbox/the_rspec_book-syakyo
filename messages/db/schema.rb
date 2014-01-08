41f5fa3093e51aea5f908f3f52b14f7b8e3466a6
thinkAmi <dev.thinkami@gmail.com>
2014-01-08 21:26:16 +0000

    p378-2

--- messages/db/schema.rb
+++ messages/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140108210604) do
+ActiveRecord::Schema.define(:version => 20140108212429) do
 
   create_table "messages", :force => true do |t|
     t.string   "title"
@@ -21,10 +21,17 @@ ActiveRecord::Schema.define(:version => 20140108210604) do
     t.integer  "sender_id"
   end
 
+  create_table "subscriptions", :force => true do |t|
+    t.integer  "subscription_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", :force => true do |t|
     t.string   "login"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "subscription_id"
   end
 
 end
