187cc0502865233e80faa8758263428ebf327edf
thinkAmi <dev.thinkami@gmail.com>
2014-01-08 20:27:03 +0000

    p371-1-1

--- messages/spec/models/user_spec.rb
+++ messages/spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#send_message" do
+    it "sends a message to another user"
+  end
 end
