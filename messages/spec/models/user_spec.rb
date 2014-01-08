537417470238d7729acdbb664d44914768d6188c
thinkAmi <dev.thinkami@gmail.com>
2014-01-08 20:34:55 +0000

    p372-1-1

--- messages/spec/models/user_spec.rb
+++ messages/spec/models/user_spec.rb
@@ -3,7 +3,12 @@ require 'spec_helper'
 describe User do
   describe "#send_message" do
     context "when the user is under their subscription limit" do
-      it "sends a message to another user"
+      it "sends a message to another user" do
+        msg = zach.send_message(
+          :recipient => david
+        )
+        david.received_messages.should == [msg]
+      end
     end
   end
 end
